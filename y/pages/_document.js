007624dfbea3d0a5bf8cb3860e415a42faae84ae
Veselin Hr. Trayanov <1.1@gmx.com>
2022-06-19 23:53:13 +0100

    <link type='image/svg'

--- y/pages/_document.js
+++ y/pages/_document.js
@@ -6,9 +6,27 @@ class MyDocument extends Document {
           return (
                <Html>
                     <Head>
-                        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&family=East+Sea+Dokdo&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet" />
+                         <link
+                              href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500&family=East+Sea+Dokdo&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+                              rel="stylesheet"
+                         />
 
-                        <link rel='preload' as='image' href='other.jpg'  type='image/jpg' crossorigin='anonymous'/>
+                         <link
+                              rel='preload'
+                              as='image'
+                              href='other.svg'
+                              type='image/svg'
+                              crossorigin='anonymous'
+                         />
+
+                         <link 
+                              rel='preload' 
+                              as='image' 
+                              href='other.jpg' 
+                              type='image/jpg' 
+                              crossorigin='anonymous' 
+                         />
+
                     </Head>
                     <body>
                          <Main />
